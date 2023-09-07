92cdbcbeec37f22241d70e171f272cd29156ca8f
jinbobs <cjoijs111@gmail.com>
2023-09-07 20:16:24 +0900

    최진성

--- test2.py
+++ test2.py
@@ -1,12 +1,7 @@
 print("test2 김현민")
-<<<<<<< HEAD
-ptint("안요셉")
-=======
+print("안요셉")
 print("test2 김현민")
 print("test3 최진성")
 print("test3 최진성")
-
 print("test3 최진성")
 print("test3 안요셉")
->>>>>>> 28fbf98e43b321e2317c81abeb27147deaf2a71e
-
