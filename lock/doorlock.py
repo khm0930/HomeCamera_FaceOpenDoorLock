6882cce87bce42aadc32d0903993281eea2dac7e
khm0930 <khm0930@github.com>
2023-10-27 18:19:52 +0900

    user기능,웹서버 통신 기능 추가

--- lock/doorlock.py
+++ lock/doorlock.py
@@ -17,8 +17,12 @@ from firebase_admin import storage
 from uuid import uuid4
 import schedule
 from datetime import datetime
+import requests
+
+
 
 app = Flask(__name__)
+
 
 PROJECT_ID = "fir-storage-ea381"
 
@@ -31,18 +35,28 @@ firebase_app = initialize_app(cred, {
 # Firebase Storage 클라이언트를 초기화합니다. 
 bucket  = storage.bucket()
 
-def fileUpload(file):
-    blob = bucket.blob('image_store/'+file) #저장한 사진을 파이어베이스 storage의 image_store라는 이름의 디렉토리에 저장
-    #new token and metadata 설정
-    new_token = uuid4()
-    metadata = {"firebaseStorageDownloadTokens": new_token} #access token이 필요하다.
-    blob.metadata = metadata
+# def fileUpload(file):
+#     blob = bucket.blob('image_store/'+file) #저장한 사진을 파이어베이스 storage의 image_store라는 이름의 디렉토리에 저장
+#     #new token and metadata 설정
+#     new_token = uuid4()
+#     metadata = {"firebaseStorageDownloadTokens": new_token} #access token이 필요하다.
+#     blob.metadata = metadata
  
-    #upload file
-    blob.upload_from_filename(filename='/home/KHM/HomeCamera_FaceOpenDoorLock/'+file, content_type='image/png') #파일이 저장된 주소와 이미지 형식(jpeg도 됨)
-    #debugging hello
-    print("저장완료 ")
-    print(blob.public_url)
+#     #upload file
+#     blob.upload_from_filename(filename='/home/KHM/HomeCamera_FaceOpenDoorLock/'+file, content_type='image/png') #파일이 저장된 주소와 이미지 형식(jpeg도 됨)
+#     #debugging hello
+#     print("저장완료 ")
+#     print(blob.public_url)
+
+    #흑백사진 업로드
+def upload_image_to_firebase(image_path, remote_path):
+    # 이미지를 Firebase Cloud Storage에 업로드
+    bucket = storage.bucket()
+    blob = bucket.blob(remote_path)
+    blob.upload_from_filename(image_path)
+    
+    # 업로드 후 로컬 파일 삭제 (선택 사항)
+    os.remove(image_path)
 # GPIO 핀 번호 설정
 TRIG_PIN = 14
 ECHO_PIN = 15
@@ -56,15 +70,19 @@ prev_frame = None
 min_area = 1000  # 윤곽선을 감지하기 위한 최소 영역 크기 (조절 가능)
 
 capture_directory = 'lock_captures/'  # 이미지 캡처를 저장할 디렉토리
+output_directory = "test_images"  # 흑백 사진을 저장할 폴더 경로 ,도어락 카메라가 찍은 사진
 
 # 이미지 캡처를 저장할 디렉토리 생성
 if not os.path.exists(capture_directory):
     os.makedirs(capture_directory)
+# 도어락 흑백 사진 저장할 디렉토리 생성
+if not os.path.exists(output_directory):
+    os.makedirs(output_directory)
 
 last_capture_time = 0  # 마지막으로 사진을 찍은 시간을 저장하는 변수
-capture_interval = 3  # 사진 찍기 간격 (초)
+capture_interval = 5  # 사진 찍기 간격 (초)
 
-
+ 
 
 # 초음파 센서로부터 거리를 측정하는 함수
 def measure_distance():
@@ -108,12 +126,20 @@ def run_camera():
 
 
              # 거리가 15cm 미만이고 모션 감지된 경우 사진 캡처
-            if distance < 10 :
+            if distance < 5 :
                 capture_time = datetime.now().strftime("%Y%m%d%H%M%S")  # 현재 날짜와 시간을 문자열로 포맷팅
                 capture_file_path = os.path.join(capture_directory, f'{capture_time}.jpg')
                 cv2.imwrite(capture_file_path, frame)
                 time.sleep(capture_interval)
-                fileUpload(capture_file_path)
+
+                face_image_path=detect_faces(capture_file_path)
+                remote_path = f'image_store/lock_captures/{capture_time}.jpg'
+                upload_image_to_firebase(face_image_path,remote_path)
+
+
+                bw_image_path = process_and_save_image(capture_file_path)#블랙으로 바꾸고 얼굴 검출
+                remote_path = f'image_store/lock_blackcaptures/{capture_time}.jpg'  # Firebase Cloud Storage에 업로드될 경로 및 파일 이름
+                upload_image_to_firebase(bw_image_path, remote_path)
 
 
             # OpenCV 창에 비디오 스트림 표시
@@ -127,6 +153,78 @@ def run_camera():
 
     # 비디오 캡처 종료 후 OpenCV 창 닫기  
     cv2.destroyAllWindows()
+
+# Haar Cascade 얼굴 검출기 초기화
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+face_cascade2 = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+
+    #입력사진 얼굴검출
+def detect_faces(image_path):
+    # 이미지 불러오기
+    image = cv2.imread(image_path)
+    
+    # 얼굴 검출
+    faces = face_cascade2.detectMultiScale(image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    for i, (x, y, w, h) in enumerate(faces):
+        # 원본 이미지에서 얼굴 부분 추출
+        face = image[y:y+h, x:x+w]
+        
+        # 새로운 파일 이름 생성
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        new_filename = f"{current_time}{i}.jpg"
+        
+        # 얼굴 이미지 저장
+        output_path = os.path.join(output_directory, new_filename)
+        cv2.imwrite(output_path, face)
+
+    return output_path
+
+    #흑백으로 이미지 변환
+def process_and_save_image(image_path):
+    # 이미지 불러오기
+    image = cv2.imread(image_path)
+    
+    # 이미지를 흑백으로 변환
+    grayscale_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+    
+    # 얼굴 검출
+    faces = face_cascade.detectMultiScale(grayscale_image, scaleFactor=1.1, minNeighbors=5, minSize=(30, 30))
+    
+    for i, (x, y, w, h) in enumerate(faces):
+        # 원본 이미지에서 얼굴 부분 추출
+        face = image[y:y+h, x:x+w]
+        
+        # 추출된 얼굴 이미지를 흑백으로 변환
+        grayscale_face = cv2.cvtColor(face, cv2.COLOR_BGR2GRAY)
+        
+        # 새로운 파일 이름 생성
+        current_time = datetime.now().strftime("%Y%m%d%H%M%S")
+        new_filename = f"{current_time}{i}.jpg"
+        
+        # 흑백 얼굴 이미지 저장
+        output_path = os.path.join(output_directory, new_filename)
+        cv2.imwrite(output_path, grayscale_face)
+
+        send_bw_image_to_server2(output_path)
+    return output_path
+
+def send_bw_image_to_server2(image_path):
+    # 이미지 파일 열기
+    with open(image_path, 'rb') as image_file:
+        files = {'image': (image_path, image_file)}
+
+        # RESTful API 엔드포인트 및 데이터 설정
+        api_url = 'http://192.168.1.11:9099/receive_image'
+        payload = {'description': '흑백 이미지 설명'}
+        
+        # RESTful POST 요청 보내기
+        response = requests.post(api_url, data=payload, files=files)
+
+        if response.status_code == 200:
+            print("흑백 이미지가 성공적으로 서버 2로 전송되었습니다.")
+        else:
+            print("흑백 이미지 전송 실패:", response.status_code)
 
 # 이미지를 다운로드할 로컬 경로 설정
 download_folder = '/home/KHM/HomeCamera_FaceOpenDoorLock/lock/static/image'
@@ -169,10 +267,11 @@ def download_images():
 
     return render_template('visit.html',image_files=IMG_LIST)
 
+
 if __name__ == '__main__':
     # 카메라 스레드 시작
     camera_thread = threading.Thread(target=run_camera)
     camera_thread.daemon = True
     camera_thread.start()
-    app.run(host='0.0.0.0', port=9092)  # 포트 번호를 9092로 변경
+    app.run(host='0.0.0.0', port=9092)
 
--- web/web.py
+++ web/web.py
@@ -1,14 +1,20 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for,jsonify,session
 import firebase_admin
 from firebase_admin import credentials, auth, db
 import cv2
 import numpy as np
 import os
+from datetime import datetime
+
+
+
 
 app = Flask(__name__)
+app.secret_key = 'your_secret_key'  # 비밀 키 설정
+
 
 # Firebase 초기화
-cred = credentials.Certificate("/home/KHM/HomeCamera_FaceOpenDoorLock/Artifical Intelligence/serviceAccount.json")
+cred = credentials.Certificate('/home/KHM/HomeCamera_FaceOpenDoorLock/service_key/serviceAccount.json')
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://fir-storage-ea381-default-rtdb.firebaseio.com'
 })
@@ -20,6 +26,66 @@ def index():
 @app.route('/register')
 def register_index():
     return render_template('register.html')
+
+@app.route('/homecam')
+def homacam_index():
+    return render_template('homecam.html')
+
+@app.route('/user')
+def user_index():
+    return render_template('user.html')
+
+
+
+@app.route('/login', methods=['POST'])
+def login():
+    try:
+        user_data = request.get_json()  # 클라이언트에서 전송한 사용자 정보를 받음
+        email = user_data['email']
+        uid = user_data['uid']
+
+        # 사용자 정보를 세션에 저장
+        session['uid'] = uid
+        session['email'] = email
+
+        # 여기에서 사용자 정보를 처리하거나 필요한 동작을 수행할 수 있습니다.
+
+        return jsonify({'success': True, 'message': '로그인 성공'})  # 성공 응답
+    except Exception as e:
+        return jsonify({'success': False, 'message': '로그인 실패: ' + str(e)})  # 실패 응답
+
+# 사용자 엔드포인트
+@app.route('/user')
+def user_index():
+    try:
+        # 세션에서 사용자 정보를 가져옵니다.
+        uid = session.get('uid')
+        email = session.get('email')
+
+        if uid is None:
+            return jsonify({'error': '사용자 정보를 가져올 수 없습니다.'})
+
+        # 파이어베이스에서 사용자 정보 가져오기
+        user = auth.get_user(uid)
+
+        created_timestamp = user.user_metadata.creation_timestamp / 1000  # 밀리초를 초로 변환
+        created_datetime = datetime.utcfromtimestamp(created_timestamp).strftime('%Y-%m-%d %H:%M:%S')
+        user_data = {
+            'uid': user.uid,
+            'email': user.email,
+            'name': user.display_name,  # Firebase에서 설정한 사용자 이름
+            'created_at': created_datetime  # 변환된 생성 시간
+        }
+
+        return render_template('user.html', user_data=user_data)
+
+    except Exception as e:
+        return jsonify({'error': '사용자 정보를 가져올 수 없습니다.', 'message': str(e)})
+
+
+
+
+    
 
 @app.route('/register_success', methods=['POST', 'GET'])
 def register():
@@ -140,7 +206,26 @@ def register():
         
 #     return render_template('choice.html')
 
-   
+image_directory = 'test_images'
+os.makedirs(image_directory, exist_ok=True)
+# 이미지를 받을 엔드포인트 및 핸들러 함수 정의
+@app.route('/receive_image', methods=['POST'])
+def receive_image():
+    # POST 요청에서 이미지 데이터 가져오기
+    image_data = request.files['image']
+    current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    image_filename = f'received_image_{current_time}.jpg'
+
+    # 이미지를 파일로 저장
+    image_path = os.path.join(image_directory, image_filename)
+    image_data.save(image_path)
+
+    print("이미지를 받아 저장했습니다:", image_path)
+
+    # 이미지 처리 또는 분석을 여기에서 수행할 수 있습니다.
+
+    return '이미지를 성공적으로 받았습니다.'
+
 @app.route('/choice')
 def choice():
     return render_template('choice.html')
