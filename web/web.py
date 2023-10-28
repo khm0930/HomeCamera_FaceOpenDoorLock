c49493fcf9eda0a984f67f234bcf938124bd5cea
Kyung <kj10009@naver.com>
2023-10-28 21:47:16 +0900

    Deep Learning Model

--- web/web.py
+++ web/web.py
@@ -1,10 +1,15 @@
-from flask import Flask, render_template, request, redirect, url_for,jsonify,session
+from flask import Flask,render_template, request, redirect, url_for,jsonify,session
 import firebase_admin
 from firebase_admin import credentials, auth, db
 import os
 from datetime import datetime
-
-
+import cv2
+import numpy as np
+import numpy as np
+import os
+from datetime import datetime
+import base64
+import tempfile
 
 
 app = Flask(__name__)
@@ -13,7 +18,7 @@ app.secret_key = 'your_secret_key'  # 비밀 키 설정
 
 # Firebase 초기화
 
-cred = credentials.Certificate('/home/KHM/HomeCamera_FaceOpenDoorLock/service_key/serviceAccount.json')
+cred = credentials.Certificate("C:\\Users\\kj100\\OneDrive\\바탕 화면\\HomeCamera_FaceOpenDoorLock\\HomeCamera_FaceOpenDoorLock\\service_key\\serviceAccount.json")
 firebase_admin.initialize_app(cred, {
     'databaseURL': 'https://fir-storage-ea381-default-rtdb.firebaseio.com'
 })
@@ -22,7 +27,7 @@ firebase_admin.initialize_app(cred, {
 def index():
     return render_template('login.html')
 
-@app.route('/register')
+@app.route('/register',methods=['POST', 'GET'])
 def register_index():
     return render_template('register.html')
 
@@ -30,7 +35,18 @@ def register_index():
 def homacam_index():
     return render_template('homecam.html')
 
+@app.route('/faceid')
+def faceid_index():
+    return render_template('faceid.html')
 
+@app.route('/faceid.html')
+def faceid_html():
+    return render_template('faceid.html')
+
+# 비디오 스트림을 위한 라우트와 핸들러 함수 설정
+@app.route('/video_feed')
+def video_feed():
+    return app.response_class(train_face(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 @app.route('/login', methods=['POST'])
 def login():
@@ -114,115 +130,126 @@ def register():
         print(str(e))
         return "회원가입 실패: " + str(e)
     
-# # 얼굴 이미지 저장을 위한 디렉토리 생성
-# prototxt = '/home/KHM/HomeCamera_FaceOpenDoorLock/web/ai/deploy.proto.txt'
-# model = '/home/KHM/HomeCamera_FaceOpenDoorLock/web/ai/res10_300x300_ssd_iter_140000_fp16.caffemodel'
-# net = cv2.dnn.readNetFromCaffe(prototxt, model)
-# os.makedirs('train', exist_ok=True)
+# DNN 모델과 가중치 파일 경로 설정
+prototxt = 'deploy.prototxt.txt'
+model = 'res10_300x300_ssd_iter_140000_fp16.caffemodel'
+net = cv2.dnn.readNetFromCaffe(prototxt, model)
+os.makedirs('train', exist_ok=True)
 
-# #얼굴저장,얼굴 학습
-# @app.route('/faceid', methods=['GET', 'POST'])
-# def faceid_index():
-#     if request.method == 'POST':
-#         face_id = request.form.get('face_id')
-#         max_images = 100  # 최대 저장할 얼굴 이미지 수
+# 얼굴 이미지를 저장할 디렉토리 설정
+TRAIN_IMAGE_DIR = 'train'  # 이미지를 저장할 디렉토리
 
-#         # 이미지 처리 및 출력 루프
-#         face_images = []
+# 얼굴 이미지 학습
+@app.route('/faceid', methods=['POST'])
+def train_face():
+    try:
+        # 이미지 처리 및 출력 루프
+        face_images = []
 
-#         capture = cv2.VideoCapture(0)
-#         capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
-#         capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
+        capture = cv2.VideoCapture(0)
+        capture.set(cv2.CAP_PROP_FRAME_WIDTH, 320)
+        capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 240)
 
-#         image_counter = 0  # 저장된 이미지 수를 세기 위한 카운터
-#         capturing = False  # 이미지 캡처 상태
+        max_images = 5  # 최대 저장할 얼굴 이미지 수
+        capturing = False  # 이미지 캡처 상태
 
-#         while len(face_images) < max_images:
-#             ret, frame = capture.read()
-#             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        while len(face_images) < max_images:
+            ret, frame = capture.read()
+            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-#             # DNN 모델을 통한 얼굴 감지
-#             blob = cv2.dnn.blobFromImage(cv2.resize(frame, (320, 240)), 1.0, (320, 240), (104.0, 177.0, 123.0))
-#             net.setInput(blob)
-#             detections = net.forward()
+            # DNN 모델을 통한 얼굴 감지
+            blob = cv2.dnn.blobFromImage(cv2.resize(frame, (320, 240)), 1.0, (320, 720), (104.0, 177.0, 123.0))
+            net.setInput(blob)
+            detections = net.forward()
 
-#             for i in range(0, detections.shape[2]):
-#                 confidence = detections[0, 0, i, 2]
+            for i in range(0, detections.shape[2]):
+                confidence = detections[0, 0, i, 2]
 
-#                 if confidence > 0.5:
-#                     box = detections[0, 0, i, 3:7] * np.array([320, 240, 320, 240])
-#                     (startX, startY, endX, endY) = box.astype(int)
+                if confidence > 0.4:
+                    box = detections[0, 0, i, 3:7] * np.array([320, 240, 320, 240])
+                    (startX, startY, endX, endY) = box.astype(int)
 
-#                     # 감지된 얼굴 주변에 큰 사각형 표시
-#                     cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 4)
+                    # 감지된 얼굴 주변에 큰 사각형 표시
+                    cv2.rectangle(frame, (startX, startY), (endX, endY), (0, 255, 0), 4)
 
-#                     if capturing:
-#                         if len(face_images) < max_images:
-#                             face_image = gray[startY:endY, startX:endX]
-#                             if not face_image is None and not face_image.size == 0:
-#                                 # 얼굴 이미지를 저장
-#                                 current_image_count = len(face_images)
-#                                 image_filename = os.path.join('train', f'{face_id}.{current_image_count}.jpg')
-#                                 cv2.imwrite(image_filename, face_image)
-#                                 print(f'얼굴 이미지 저장: {image_filename}')
-#                                 face_images.append(face_image)  # 얼굴 이미지 저장
+                    if capturing:
+                        if len(face_images) < max_images:
+                            face_image = gray[startY:endY, startX:endX]
+                            face_image = cv2.resize(face_image, (320,320))
+                            if not face_image is None and not face_image.size == 0:
+                                # 얼굴 이미지를 저장
+                                current_image_count = len(face_images)
+                                image_filename = os.path.join(TRAIN_IMAGE_DIR, f'{current_image_count}.jpg')
+                                cv2.imwrite(image_filename, face_image)
+                                print(f'얼굴 이미지 저장: {image_filename}')
+                                face_images.append(face_image)  # 얼굴 이미지 저장
 
-#                     if len(face_images) >= max_images:
-#                         break
+                    if len(face_images) >= max_images:
+                        break
 
-#             cv2.imshow('image', frame)
-#             key = cv2.waitKey(1)
+            cv2.imshow('image', frame)
+            key = cv2.waitKey(1)
 
-#             if key > 0:
-#                 if key == ord('q'):
-#                     break
-#                 elif key == ord('s'):
-#                     # 이미지 캡처 시작 또는 종료
-#                     capturing = not capturing
+            if key > 0:
+                if key == ord('q'):
+                    break
+                elif key == ord('s'):
+                    # 이미지 캡처 시작 또는 종료
+                    capturing = not capturing
 
-#         # 카메라 종료
-#         capture.release()
-#         cv2.destroyAllWindows()
+        # 카메라 종료
+        capture.release()
+        cv2.destroyAllWindows()
 
-#         # train 폴더에 저장된 얼굴 이미지를 사용하여 학습 수행
-#         if len(face_images) > 0:
-#             labels = np.array([0] * len(face_images))
-#             recognizer = cv2.face_LBPHFaceRecognizer.create()
-#             recognizer.train(face_images, labels)
-#             model_file = 'trained_model.yml'
-#             recognizer.save(model_file)
-#             print("모델:",model_file)
+        # train 폴더에 저장된 얼굴 이미지를 사용하여 학습 수행
+        if len(face_images) > 0:
+            labels = np.array([0] * len(face_images))
+            recognizer = cv2.face_LBPHFaceRecognizer.create()
+            recognizer.train(face_images, labels)
+            model_file = 'trained_model.yml'
+            recognizer.save(model_file)
+            return jsonify({'message': '학습이 완료되었습니다.'}), 200
+            
+        else:
+            # 에러 메시지도 JSON 형식으로 포함합니다.
+            return jsonify({'message': '학습을 위한 얼굴 이미지가 캡처되지 않았습니다.'}), 400
 
-#             return f'학습이 완료되었습니다. 모델이 {model_file}로 저장되었습니다.'
-#         else:
-#             return '학습을 위한 얼굴 이미지가 캡처되지 않았습니다.'
-        
-#     return render_template('choice.html')
+    except Exception as e:
+        return f'학습 도중 오류가 발생했습니다: {str(e)}'
 
-image_directory = 'test_images'
-os.makedirs(image_directory, exist_ok=True)
-# 이미지를 받을 엔드포인트 및 핸들러 함수 정의
-@app.route('/receive_image', methods=['POST'])
-def receive_image():
-    # POST 요청에서 이미지 데이터 가져오기
-    image_data = request.files['image']
-    current_time = datetime.now().strftime('%Y%m%d%H%M%S')
-    image_filename = f'received_image_{current_time}.jpg'
+@app.route('/train_complete')
+def train_complete():
+    return render_template('train_complete.html')
 
-    # 이미지를 파일로 저장
-    image_path = os.path.join(image_directory, image_filename)
-    image_data.save(image_path)
-
-    print("이미지를 받아 저장했습니다:", image_path)
-
-    # 이미지 처리 또는 분석을 여기에서 수행할 수 있습니다.
-
-    return '이미지를 성공적으로 받았습니다.'
 
 @app.route('/choice')
 def choice():
     return render_template('choice.html')
 
-if __name__ == '__main__':
+@app.route('/faceid')
+def face():
+    return render_template('faceid.html')
 
-    app.run(host='0.0.0.0', port=9092)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5123)
+
+
+# image_directory = 'test_images'
+# os.makedirs(image_directory, exist_ok=True)
+# # 이미지를 받을 엔드포인트 및 핸들러 함수 정의
+# @app.route('/receive_image', methods=['POST'])
+# def receive_image():
+#     # POST 요청에서 이미지 데이터 가져오기
+#     image_data = request.files['image']
+#     current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+#     image_filename = f'received_image_{current_time}.jpg'
+
+#     # 이미지를 파일로 저장
+#     image_path = os.path.join(image_directory, image_filename)
+#     image_data.save(image_path)
+
+#     print("이미지를 받아 저장했습니다:", image_path)
+
+#     # 이미지 처리 또는 분석을 여기에서 수행할 수 있습니다.
+
+#     return '이미지를 성공적으로 받았습니다.'
